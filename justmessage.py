5ac9be4d66144daf157113a4f0514504d63f6ca4
July Tikhonov <july.tikh@gmail.com>
2013-05-29 23:03:29 +0400

    Enable python scripts to set terminal caption.

--- justmessage.py
+++ justmessage.py
@@ -7,7 +7,14 @@ def main(message, font='Monospace 30', title='just a message'):
     label = Label(root, text=message, font=font).pack()
     root.mainloop()
 
+def set_terminal_caption():
+    import sys
+    sys.stdout.write('\033]2;' + sys.argv[0].rpartition('/')[2] + '\007')
+    sys.stdout.flush()
+
 if __name__ == '__main__':
+    set_terminal_caption()
+
     import argparse
     parser = argparse.ArgumentParser(prog='justmessage', description="just display a message")
     parser.add_argument('message')
